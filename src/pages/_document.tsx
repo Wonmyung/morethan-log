e11213451fcf1b0eb906abf066a1efa163ba1cec
Wonmyung Lee <wonmyunglee92@gmail.com>
2024-02-01 14:27:03 +0900

    Update logo and icon images

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,9 +8,9 @@ class MyDocument extends Document {
         <Head>
           <link rel="icon" href="/favicon.ico" />
           <link
-            rel="apple-touch-icon"
+            rel="apple-icon"
             sizes="192x192"
-            href="/apple-touch-icon.png"
+            href="/apple-icon.png"
           ></link>
           <link
             rel="alternate"
